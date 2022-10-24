05a397d03b39f81c14f8863a9a3994908f874dc3
Sean <millington.sean12@gmail.com>
2022-10-24 17:45:08 +0200

    added login and request functions

---
+++ index.js
@@ -0,0 +1,27 @@
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', requestLogin);
+
+async function requestLogin(e) {
+    e.preventDefault();
+    try {
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
+        }
+        const r = await fetch(`http://localhost:3000/auth/login`, options)
+        const data = await r.json()
+        if (data.err){ throw Error(data.err); }
+        login(data);
+    } catch (err) {
+        console.warn(`Error: ${err}`);
+    }
+}
+
+function login(data){
+    const payload = jwt_decode(data.token);
+    localStorage.setItem('token', data.token)
+    localStorage.setItem('username', payload.username)
+    localStorage.setItem('email', payload.email)
+    location.href = './dashboard.html';
+}
---
+++ register.js
@@ -0,0 +1,19 @@
+const registerForm = document.getElementById('RegisterForm');
+loginForm.addEventListener('submit', requestRegistration);
+
+async function requestRegistration(e) {
+    e.preventDefault();
+    try {
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
+        }
+        const r = await fetch(`http://localhost:3000/auth/register`, options)
+        const data = await r.json()
+        if (data.err){ throw Error(data.err) }
+        requestLogin(e);
+    } catch (err) {
+        console.warn(err);
+    }
+}

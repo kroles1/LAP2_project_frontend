551cde0dac1d9c538648d902f797dfddf277d4b0
aimyv <av533@bath.ac.uk>
2022-10-26 14:49:51 +0100

    changed dashboard.js, editHabit.js to new routes

--- editHabit.js
+++ editHabit.js
@@ -4,11 +4,13 @@ editHabitForm.addEventListener('submit', editHabit);
 const Delete = document.getElementById('delete');
 Delete.addEventListener('click', deleteHabit);
 
+const id = localStorage.getItem('id');
+
 async function deleteHabit(e) {
     e.preventDefault();
     if(confirm('Are you sure you want to delete this habit?')) {
         try {
-            const r = await fetch(`http://localhost:3000/habits/${id}`, {
+            const r = await fetch(`https://track-it-backend.onrender.com/habits/${id}`, {
                 method: 'DELETE',
             });
             if (r.err){ 
@@ -24,10 +26,9 @@ async function deleteHabit(e) {
 
 async function editHabit(e) {
     e.preventDefault();
-    const id = localStorage.getItem('id');
     try {
         const options = {
-            method: 'PATCH',
+            method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 name: e.target.name.value,
@@ -36,7 +37,7 @@ async function editHabit(e) {
                 number_of_rep: e.target.reps.value
             })
         }
-        const r = await fetch(`http://localhost:3000/habits/${id}`, options)
+        const r = await fetch(`https://track-it-backend.onrender.com/habits/${id}`, options)
         const data = await r.json()
         if (data.err){ throw Error(data.err); }
     } catch (err) {
